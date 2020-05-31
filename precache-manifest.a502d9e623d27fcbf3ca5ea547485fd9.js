797bf4bc2571fcf511f3e5e4976c27af5e1c1d7d
desconocido-odiconocsed <desconocido.odiconocsed@protonmail.com>
2020-05-31 02:09:41 -0400

    Updates

---
+++ precache-manifest.a502d9e623d27fcbf3ca5ea547485fd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f116b4a17de74d2bdb7eeab9531861",
+    "url": "/Antastic-Prod/index.html"
+  },
+  {
+    "revision": "21748fbf1f2c7c52e14e",
+    "url": "/Antastic-Prod/static/css/main.ad9da7db.chunk.css"
+  },
+  {
+    "revision": "64d67bbaf943dd227ce1",
+    "url": "/Antastic-Prod/static/js/2.45d273df.chunk.js"
+  },
+  {
+    "revision": "81bce0ee90f77330fc8e46c3a6e1f9cd",
+    "url": "/Antastic-Prod/static/js/2.45d273df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21748fbf1f2c7c52e14e",
+    "url": "/Antastic-Prod/static/js/main.ae76d043.chunk.js"
+  },
+  {
+    "revision": "9d1389d4103d87962745",
+    "url": "/Antastic-Prod/static/js/runtime-main.e50532fa.js"
+  },
+  {
+    "revision": "31298dd8cfae0bfff8db9374cdf09e6f",
+    "url": "/Antastic-Prod/static/media/markets.31298dd8.png"
+  }
+]);
